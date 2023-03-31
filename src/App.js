aabb14dd86efd1d2370c4da1be5be7bbec0460cb
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-03-31 16:28:42 +0200

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { useState, useTransition } from "react";
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const LIST_SIZE = 20000;
+  function handleChange(e) {
+    setInput(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let index = 0; index < LIST_SIZE; index++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
+  }
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending
+        ? "load"
+        : list.map((item, index) => <div key={index}>{item}</div>)}
+    </>
   );
 }
 
